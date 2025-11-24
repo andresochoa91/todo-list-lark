6077577739f08ab944dcc544fc3a8cbaad206a9b
andresochoa91 <andres_ochoa91@hotmail.com>
2025-11-24 07:15:45 -0800

    week 2

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import { todos } from './App';
+
+function TodoList() {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
