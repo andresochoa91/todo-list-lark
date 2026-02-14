32bbf4161828b50855135de45100797c5c2212c4
andresochoa91 <andres_ochoa91@hotmail.com>
2026-02-14 08:43:22 -0800

    React-Router Routing And Pagination

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,22 +1,62 @@
 import TodoListItem from './TodoListItem';
 import Style from './TodoList.module.css';
+import { useSearchParams } from 'react-router';
 
 function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
   const filteredTodoList = todoList.filter((todo) => todo.isCompleted !== true);
 
-  return filteredTodoList.length ? (
-    <ul className={`${Style.padding} ${Style.noBullet}`}>
-      {filteredTodoList.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          key={todo.id}
-          onCompleteTodo={onCompleteTodo}
-          onUpdateTodo={onUpdateTodo}
-        />
-      ))}
-    </ul>
-  ) : (
-    <p>Add todo above to get started</p>
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const itemsPerPage = 4;
+
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+
+  const indexOfFirstTodo = (currentPage - 1) * itemsPerPage;
+
+  const totalPages = Math.ceil(filteredTodoList.length / itemsPerPage);
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setSearchParams((params) => {
+        params.set('page', currentPage - 1);
+        return params;
+      });
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setSearchParams((params) => {
+        params.set('page', currentPage + 1);
+        return params;
+      });
+    }
+  };
+
+  return (
+    <>
+      {filteredTodoList.length ? (
+        <ul className={`${Style.padding} ${Style.noBullet}`}>
+          {filteredTodoList
+            .slice(indexOfFirstTodo, indexOfFirstTodo + itemsPerPage)
+            .map((todo) => (
+              <TodoListItem
+                todo={todo}
+                key={todo.id}
+                onCompleteTodo={onCompleteTodo}
+                onUpdateTodo={onUpdateTodo}
+              />
+            ))}
+        </ul>
+      ) : (
+        <p>Add todo above to get started</p>
+      )}
+      <div>
+        <button onClick={handlePreviousPage}>Previous</button>
+        {`Page: ${currentPage} of ${totalPages}`}
+        <button onClick={handleNextPage}>Next</button>
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import AppContext from './AppContext.jsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppContext />
+    <BrowserRouter>
+      <AppContext />
+    </BrowserRouter>
   </StrictMode>
 );
